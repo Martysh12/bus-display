679dacbc2f8022fd90012ca2ae0944f021e52b39
Martysh12 <49569238+Martysh12@users.noreply.github.com>
2022-02-16 15:16:54 +0200

    Changed the number to a displayable one

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 
 int main(int argc, char* argv[])
 {
-    std::cout << stringify_disp_num(draw_disp_num(1234)) << std::endl;
+    std::cout << stringify_disp_num(draw_disp_num(420)) << std::endl;
     return 0;
 }
 
